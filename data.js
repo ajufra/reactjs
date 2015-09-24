795d2790d5a9bc3d30369a7534b0501100239655
afranco@swat-it.co <afranco@swat-it.co>
2015-09-24 15:51:27 -0500

    First commit

---
+++ data.js
@@ -0,0 +1,12 @@
+var employees=[
+{	name:'Andrés Franco',		id: '001'},
+{	name:'Andres Giraldo',		id: '002'},
+{	name:'David Restrepo',		id: '003'},
+{	name:'Eliecer Perez',		id: '004'},
+{	name:'Danithza Restrepo',	id: '005'},
+{	name:'Alejandro Ocampo',	id: '006'},
+{	name:'Elizabeth',			id: '007'},
+{	name:'Carlos Arcila',		id: '008'},
+{	name:'Carolina Marulanda',	id: '009'},
+{	name:'Callejas',			id: '010'},
+]
